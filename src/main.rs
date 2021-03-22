bd8fcc441702d3d4f9a306bc0a9491a89fc2d688
wangju <wangju@shein.com>
2021-03-22 22:36:45 +0800

    :pushpin: tide server start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tide::log;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    log::start();
+    let mut app = tide::new();
+    app.at("/").get(|_| async {
+        Ok("hello world")
+    });
+    log::info!("app is running");
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
